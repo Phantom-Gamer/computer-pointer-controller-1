3683068cd92751c6e2b3b47debb2f2221f2f5813
Gaurav Yadav <gaurav.yadav@webklipper.com>
2020-05-03 14:16:54 +0530

    Add results

--- README.md
+++ README.md
@@ -24,23 +24,55 @@ You will have to coordinate the flow of data from the input, and then amongst th
 
 
 ## Documentation
+Enable the virtual environment:
+
+```
+source /opt/intel/openvino/bin/setupvars.sh
+```
+
+Install the requirements:
+
+```
+pip install -e requirements.txt
+```
+
 * Run command `python3 main.py -h` to get the supported arguments
 * Inference Engine API Docs [here](https://docs.openvinotoolkit.org/latest/_inference_engine_ie_bridges_python_docs_api_overview.html)
 * Model documentation [here](https://docs.openvinotoolkit.org/latest/_models_intel_index.html) 
 
 ## Benchmarks
-I am not able to run the model on multiple devices because I don't have access to all kind of hardware. Though I did try to execute this on udacity's workspace but was getting `file not found ` erro for input file.
+I run this model on 4 different hardware using Intel-dev cloud. Below is the list of hardware used and their results.
+CPU: https://ark.intel.com/products/88186/Intel-Core-i5-6500TE-Processor-6M-Cache-up-to-3-30-GHz-
+IGPU: https://ark.intel.com/products/88186/Intel-Core-i5-6500TE-Processor-6M-Cache-up-to-3-30-GHz-
+VPU: https://software.intel.com/en-us/neural-compute-stick
+FPGA: https://www.ieiworld.com/mustang-f100/en/
+
+Results with Model Precision-FP32:
+FPS Graph                  |  Model load time plot
+:-------------------------:|:-------------------------:
+![](images/fp32_frames_ps.png)  |  ![](images/fp32_model_load__time.png)
+
+Inference time plot        |  Combine graph for all 
+:-------------------------:|:-------------------------:
+![](images/fp32_inference_time.png)  |  ![](images/fp32_comparison.png)
+
+
+Results with Model Precision-FP16:
+FPS Graph                  |  Model load time plot
+:-------------------------:|:-------------------------:
+![](images/fp16_frames_ps.png)  |  ![](images/fp16_model_load__time.png)
+
+Inference time plot        |  Combine graph for all 
+:-------------------------:|:-------------------------:
+![](images/fp16_inference_time.png)  |  ![](images/fp16_comparison.png)
+
 
 NOTE: I have attached the expected working python notebook to run this experiment on multiple devices. Check it [here](src/computer-controller.ipynb)
 
 ## Results
-**System Info:**
-MacBook Pro with intel i5
-Inference time for full demo video:
+As clear from the above results, total inference time and model load time is almost equal for all the device.
 
-    FP16: 113.7 sec
-    FP32: 117.2 sec
-
+To run this model on intel-dev-cloud platform, use this [notebook](src/computer-controller.ipynb) and follow the guidlines from [how-to-run](how-to-run.md)
 
 ## Stand Out Suggestions
 This is where you can provide information about the stand out suggestions that you have attempted.
--- src/queue_job.sh
+++ src/queue_job.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-exec 1>/output/stdout.log 2>/output/stderr.log
+exec 1>output/stdout.log 2>output/stderr.log
 
 mkdir -p $1
 OUTPUT_FILE=$1
 DEVICE=$2
-FP_MODEL=$3
+PRECISION=$3
 INPUT_FILE=$4
 THRESHOLD=$5
 
-if [ "$FP_MODEL" = "FP32" ]; then
-    FACEMODELPATH=/data/models/intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-binary-0001.xml
-    POSEMODELPATH=/data/models/intel/head-pose-estimation-adas-0001/FP32/head-pose-estimation-adas-0001.xml
-    LANDMARKSMODELPATH=/data/models/intel/landmarks-regression-retail-0009/FP32/landmarks-regression-retail-0009.xml    
-    GAZEMODELPATH=/data/models/intel/gaze-estimation-adas-0002/FP32/gaze-estimation-adas-0002.xml        
+if [ "$PRECISION" = "FP32" ]; then
+    FACEMODELPATH=../models/intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-binary-0001.xml
+    POSEMODELPATH=../models/intel/head-pose-estimation-adas-0001/FP32/head-pose-estimation-adas-0001.xml
+    LANDMARKSMODELPATH=../models/intel/landmarks-regression-retail-0009/FP32/landmarks-regression-retail-0009.xml    
+    GAZEMODELPATH=../models/intel/gaze-estimation-adas-0002/FP32/gaze-estimation-adas-0002.xml        
 else
-    FACEMODELPATH=/data/models/intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-binary-0001.xml
-    POSEMODELPATH=/data/models/intel/head-pose-estimation-adas-0001/FP16/head-pose-estimation-adas-0001.xml
-    LANDMARKSMODELPATH=/data/models/intel/landmarks-regression-retail-0009/FP16/landmarks-regression-retail-0009.xml    
-    GAZEMODELPATH=/data/models/intel/gaze-estimation-adas-0002/FP16/gaze-estimation-adas-0002.xml        
+    FACEMODELPATH=../models/intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-binary-0001.xml
+    POSEMODELPATH=../models/intel/head-pose-estimation-adas-0001/FP16/head-pose-estimation-adas-0001.xml
+    LANDMARKSMODELPATH=../models/intel/landmarks-regression-retail-0009/FP16/landmarks-regression-retail-0009.xml    
+    GAZEMODELPATH=../models/intel/gaze-estimation-adas-0002/FP16/gaze-estimation-adas-0002.xml        
 fi
 
 if echo "$DEVICE" | grep -q "FPGA"; then # if device passed in is FPGA, load bitstream to program FPGA
@@ -27,9 +27,7 @@ if echo "$DEVICE" | grep -q "FPGA"; then # if device passed in is FPGA, load bit
     aocl program acl0 /opt/intel/openvino/bitstreams/a10_vision_design_sg1_bitstreams/2019R4_PL1_FP16_MobileNet_Clamp.aocx
 fi
 
-ls /data/*
-
-python3 main.py -fm ${FACEMODELPATH} \
+python3 main-intel-dev.py -fm ${FACEMODELPATH} \
                 -pm ${POSEMODELPATH} \
                 -lm ${LANDMARKSMODELPATH} \
                 -gm ${GAZEMODELPATH} \
@@ -37,7 +35,3 @@ python3 main.py -fm ${FACEMODELPATH} \
                 -o ${OUTPUT_FILE} \
                 -d ${DEVICE} \
                 -c ${THRESHOLD}
-
-cd /output
-
-tar zcvf output.tgz *
